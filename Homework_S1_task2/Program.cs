68866870525355efc0282edaf1a9ab5e7ac3ba1d
Igor <igk85@list.ru>
2022-11-22 21:39:45 +0300

    task2

---
+++ Homework_S1_task2/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x, y, z;//создали три целых числа
+        int max;
+        Console.WriteLine("Введите три числа : ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        y = Convert.ToInt32(Console.ReadLine());
+
+        z = Convert.ToInt32(Console.ReadLine());
+
+
+        if (x >= y)
+        {
+            if (z >= x)
+            {
+                max = z;
+            }
+            else
+            {
+                max = x;
+            }
+        }
+        else
+        {
+            if (z >= y)
+            {
+                max = z;
+            }
+            else
+            {
+                max = y;
+            }
+            Console.Write("Наибольшее число ");
+            Console.WriteLine(max);
+        }
+    }
+}
