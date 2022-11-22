ebeef8905b3a08d9bbb4afa720026aeb339719f7
Igor <igk85@list.ru>
2022-11-22 22:06:13 +0300

    task3

---
+++ Homework_S1_task3/Program.cs
@@ -0,0 +1,16 @@
+﻿int x;
+            Console.Write("Введите число : ");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+ 
+                Console.WriteLine("Введенное число является четным");
+ 
+            }
+ 
+            else
+ 
+            {
+ 
+                Console.WriteLine("Введенное число нечетное");
+            }    
