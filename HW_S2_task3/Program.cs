bab325a986a1d80f4070f0fceef394a4bc315bc2
Igor <igk85@list.ru>
2022-11-23 22:25:39 +0300

    task3

---
+++ HW_S2_task3/Program.cs
@@ -0,0 +1,9 @@
+﻿int N;
+Console.Write("Введите число от 1 до 7: ");
+        N = Convert.ToInt32(Console.ReadLine());
+            if ((N >= 1) && (N<= 7))
+            {
+                if (N== 6 | N == 7) Console.WriteLine("Выходной день");
+                else Console.WriteLine("Рабочий день");
+            }
+            else Console.WriteLine("Не верное число");
