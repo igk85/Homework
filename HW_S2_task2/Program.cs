a725983b0d8292bf49a19155822c61b349c0c42f
Igor <igk85@list.ru>
2022-11-24 22:07:58 +0300

    task203

--- HW_S2_task2/Program.cs
+++ HW_S2_task2/Program.cs
@@ -1,34 +1,15 @@
-﻿int number = ReadInt("Введите число: ");
-int count = number.ToString().Length;
-Console.Write(MakeArray(number, count));
+﻿int N , M, S;
 
-
-// ФУНКЦИИ------------------------------------------------------------------------------------------------------
-
-// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-// Функция принимает число введенное пользователем, количество символов, и выводит третью цифру числа. Если 3 цифры нет, сообщает и выводит 0.
-int MakeArray(int a, int b)
-{
-int result = 0;
-    if (b < 3)
-    {
-        Console.Write("Третьей цифры нет, держи: ");
-    }
-    else
-    {
-        int c = 1;
-        for (int i = b; i > 3; i--)
+Console.Write("Введите любое число : ");
+        N = Convert.ToInt32(Console.ReadLine());
+        M= N.ToString().Length;
+        if((M <= 2))
         {
-            c = c * 10;
+            Console.WriteLine("Третьей цифры нет ");
         }
-
-        result = (a / c) % 10;
-    }
-return result;
-}
+        else 
+        {
+            S = (N / Convert.ToInt32(Math.Pow(10, M - 3)) % 10);
+            Console.WriteLine("Всего цифр {0}", M);
+            Console.WriteLine("{0} третье число ", S);
+        }
