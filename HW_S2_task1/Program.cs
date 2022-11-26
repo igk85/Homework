0bc4a52feb706acf7af52dbab4403dbf62e4d37f
Igor <igk85@list.ru>
2022-11-26 20:11:05 +0300

    task105

--- HW_S2_task1/Program.cs
+++ HW_S2_task1/Program.cs
@@ -3,15 +3,10 @@ Console.Write("Введите число N: ");
         N = Convert.ToInt32(Console.ReadLine());
         if((N < 100)^(N > 999))
         {
-
             Console.WriteLine("{0} Не является трехзначным числом ",N);
-
         }
-
         else
-
         {
             S=(N / 10) % 10;
             Console.WriteLine("{0} второе число ", S);
-
         }
