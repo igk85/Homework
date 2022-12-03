1d888120d4c78b2c7ec431129d0759d456f2c1ce
Igor <igk85@list.ru>
2022-12-03 11:34:48 +0300

    Sem3

---
+++ Seminars/task24/Program.cs
@@ -0,0 +1,14 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+Console.Write("Введите число N: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("");
+    int x;
+    int i = 1;
+     while (i <= N)
+     {
+        x = i * i  * i;
+        i++;
+        Console.WriteLine(" "+ x);
+     }
+
