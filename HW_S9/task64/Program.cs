ef0dda444335a64fb559eb7b89aa8aa3cc44162e
Igor <igk85@list.ru>
2022-12-06 22:29:13 +0300

    Semenar9

---
+++ HW_S9/task64/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NatToLow(number, count);
+
+
+void NatToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NatToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
---
+++ HW_S9/task66/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(M, N);
+
+// Вызов функции
+void SumFromMToN(int M, int N)
+{
+    Console.WriteLine(Sum(M - 1, N));
+}
+
+// Функция сумма чисел от M до N
+int Sum(int M, int N)
+{
+    int res = M;
+    if (M == N)
+        return 0;
+    else
+    {
+        M++;
+        res = M + Sum(M, N);
+        return res;
+    }
+}
